fff27854418b0b74c76f28d28386c2b0661b66e3
Robson Paulo <65135918+rabisquim@users.noreply.github.com>
2023-06-08 17:19:41 -0300

    Update Tarefa.java

--- src/br/com/robson/todolist/modelos/Tarefa.java
+++ src/br/com/robson/todolist/modelos/Tarefa.java
@@ -1,2 +1,23 @@
-package br.com.robson.todolist.modelos;public class Tarefa {
+package br.com.robson.todolist.modelos;
+
+public class Tarefa {
+    private String descricao;
+    private boolean concluida;
+
+    public Tarefa (String descricao) {
+
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public boolean foiConcluida() {
+        return concluida;
+    }
+    public void marcarConcluida() {
+        this.concluida = true;
+    }
+
 }
