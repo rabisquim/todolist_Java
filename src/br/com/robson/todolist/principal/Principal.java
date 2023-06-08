4ff092254f7a8da609b91ed6d63325db373aecff
Robson Paulo <rabisquim@gmail.com>
2023-06-08 16:46:40 -0300

    Initial commit

---
+++ src/br/com/robson/todolist/principal/Principal.java
@@ -0,0 +1,12 @@
+package br.com.robson.todolist.principal;
+
+public class GerenciadorTarefasApp {
+    public static void main(String[] args) {
+        System.out.println("Bem-vindo ao Gerenciador de Tarefas!");
+
+        Menu menu = new Menu();
+        menu.exibir();
+    }
+}
+
+
